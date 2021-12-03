9d86d5105eab7a4c516c3793752235d7788c0e6e
avilamowski <avilamowski@itba.edu.ar>
2021-12-03 19:43:45 -0300

    Creacion de title.h

---
+++ csvADT.c
@@ -0,0 +1,12 @@
+//
+// Created by mbp13 on 03/12/2021.
+//
+
+#include <stdio.h>
+#include "csvADT.h"
+
+typedef struct csvCDT {
+    FILE * file;
+} csvCDT;
+
+
---
+++ csvADT.h
@@ -0,0 +1,8 @@
+//
+// Created by mbp13 on 03/12/2021.
+//
+
+#ifndef TPE_G1_CSVADT_H
+#define TPE_G1_CSVADT_H
+
+#endif //TPE_G1_CSVADT_H
